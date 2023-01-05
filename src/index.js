049558f5e85852c9b50886e79b4902b169bb1bb2
Alexey197 <margadonnn3@gmail.com>
2023-01-05 12:46:17 +0300

    pre-starting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
+import reportWebVitals from './reportWebVitals'
 
 const element = <h1>Hello World</h1>
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(element)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
