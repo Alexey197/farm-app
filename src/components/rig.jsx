0b86403c9775e5cea7dffe0f2b2ccdfa37983ddd
Алексей Даньев <aleksejdanev@MacBook-Pro-Aleksej-2.local>
2023-01-21 20:11:35 +0700

    delete button

--- src/components/rig.jsx
+++ src/components/rig.jsx
@@ -4,6 +4,11 @@ import {tableParameters} from '../api/card.api'
 const Rig = ({rig, rigNumber}) => {
     const [cards, setCards] = useState(rig)
     const updatedTableParam = tableParameters.filter(params => params !== [...tableParameters].shift())
+    const handleDelete = (e) => {
+        console.log(e.target.closest("tr"));
+        e.target.closest("tr").remove()
+        // setCards(cards.filter(card => card === card))
+    }
     return (
         <>
             <h2 className="col-md-1 mx-auto">Rig-{rigNumber}</h2>
@@ -23,9 +28,17 @@ const Rig = ({rig, rigNumber}) => {
                         <td>{cardIndex + 1}</td>
                         <td suppressContentEditableWarning={true}>{card}</td>
                         {updatedTableParam.map((_, paramIndex) => (
-                            <td id={`${rigNumber}:${cardIndex + 1}:${paramIndex + 1}`} key={paramIndex} contentEditable={"true"}></td>
+                            <td id={`${rigNumber}:${cardIndex + 1}:${paramIndex + 1}`} key={paramIndex}
+                                contentEditable={"true"}></td>
                         ))}
-                        <td>Remove</td>
+                        <td>
+                            <button
+                                className={"btn btn-danger"}
+                                onClick={(e) => handleDelete(e)}
+                            >
+                                Удалить
+                            </button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
